dc2cc195004ae7f01958a6a97c391c2a891a9f54
tayamba mwanza <tayambamwanza@gmail.com>
2020-11-16 14:34:56 +0200

    refactor: if statement curly braces

--- projects/ngx-bootstrap-icons-lib/src/lib/components/ngx-bootstrap-icons.component.ts
+++ projects/ngx-bootstrap-icons-lib/src/lib/components/ngx-bootstrap-icons.component.ts
@@ -1,7 +1,12 @@
 import {
-  ChangeDetectorRef, Component, ElementRef, Inject, Input, OnChanges, SimpleChanges,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Inject,
+  Input,
+  OnChanges,
+  SimpleChanges,
 } from '@angular/core';
-
 import { Icons } from '../providers/icon.provider';
 import { uppercamelcase } from '../utils/utils';
 
@@ -17,16 +22,25 @@ export class NgxBootstrapIconsLibComponent implements OnChanges {
 
   @Input() height!: string;
 
-  constructor(private elem: ElementRef, private changeDetector: ChangeDetectorRef, @Inject(Icons) private icons: Icons) { }
+  constructor(
+    private elem: ElementRef,
+    private changeDetector: ChangeDetectorRef,
+    @Inject(Icons) private icons: Icons
+  ) {}
 
-  ngOnChanges(changes: SimpleChanges) {
+  ngOnChanges(changes: SimpleChanges): void {
     // icons are provided as an array of objects because of "multi: true"
-    const icons = Object.assign({}, ...(this.icons as any as object[]));
+    const icons = Object.assign({}, ...((this.icons as any) as object[]));
     let svg = icons[uppercamelcase(changes.name.currentValue)] || '';
 
     if (!svg) console.warn(`Icon not found: ${changes.name.currentValue}\n`);
-    if (this.width && svg.includes('width')) svg = svg.replace('width="1em"', `width="${this.width}"`);
-    if (this.height && svg.includes('height')) svg = svg.replace('height="1em"', `height="${this.height}"`);
+    if (this.width && svg.includes('width')) {
+      svg = svg.replace('width="1em"', `width="${this.width}"`);
+    }
+
+    if (this.height && svg.includes('height')) {
+      svg = svg.replace('height="1em"', `height="${this.height}"`);
+    }
 
     this.elem.nativeElement.innerHTML = svg;
     this.changeDetector.markForCheck();
