da78dd741f15c259e34dd5564f8a4c1658355d1c
tayamba mwanza <tayambamwanza@gmail.com>
2020-11-16 17:31:25 +0200

    feat: import from source

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,27 @@ import ru from '@angular/common/locales/ru';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { allIcons, NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
 import { ClipboardModule } from 'ngx-clipboard';
+import {
+  allIcons,
+  NgxBootstrapIconsModule,
+} from '../../projects/ngx-bootstrap-icons-lib/src/public-api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 registerLocaleData(ru);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     FormsModule,
     CommonModule,
     AppRoutingModule,
     NgxBootstrapIconsModule.pick(allIcons),
-    ClipboardModule
+    ClipboardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
